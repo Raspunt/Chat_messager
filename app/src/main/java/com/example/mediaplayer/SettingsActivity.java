f333f6b414a73dc1807259e2fa0de1fe85fae940
Raspunt <m_ryabinin@inbox.ru>
2022-04-27 20:40:22 +0600

    new funcs

--- app/src/main/java/com/example/mediaplayer/SettingsActivity.java
+++ app/src/main/java/com/example/mediaplayer/SettingsActivity.java
@@ -1,15 +1,23 @@
 package com.example.mediaplayer;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.documentfile.provider.DocumentFile;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.DocumentsContract;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
 
-
+    TextView tv ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +28,7 @@ public class SettingsActivity extends AppCompatActivity {
 
          Button MainPageBtn = findViewById(R.id.main_pageBtn);
          Button ChangeMusicFolder = findViewById(R.id.ChangeMusicFolder);
+         tv = findViewById(R.id.pathToDir);
 
 
 
@@ -28,13 +37,19 @@ public class SettingsActivity extends AppCompatActivity {
              @Override
              public void onClick(View view) {
                  Intent intent = new Intent()
-                         .setType("*/*")
-                         .setAction(Intent.ACTION_GET_CONTENT);
-                 startActivityForResult(Intent.createChooser(intent, "Select a file"), 123);
+                         .setAction(Intent.ACTION_OPEN_DOCUMENT_TREE);
+
+                 intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, "/");
+
+                 startActivityForResult(Intent.createChooser(intent, "Select a file"),224);
+
+
+
+
+
+
              }
          });
-
-
 
         MainPageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,13 +58,15 @@ public class SettingsActivity extends AppCompatActivity {
                 Intent intent = new Intent(view.getContext(),MainActivity.class);
                 startActivity(intent);
 
+
             }
         });
-
     }
 
-
-
-
-    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        tv.setText(data.getDataString());
+        MainActivity.music_folder = data.getDataString();
+    }
 }
